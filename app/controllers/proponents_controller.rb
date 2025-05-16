3f5373f9b806a8b11bfbf8744e6f312a2c8b80a8
rodrigo almeida <rodrigo.rsaal@gmail.com>
2025-05-16 18:08:35 -0300

    Implementing discount

--- app/controllers/proponents_controller.rb
+++ app/controllers/proponents_controller.rb
@@ -1,2 +1,37 @@
 class ProponentsController < ApplicationController
-end
+    def new
+      @proponent = Proponent.new
+      @proponent.addresses.build
+      @proponent.contacts.build
+    end
+  
+    def create
+      @proponent = Proponent.new(proponent_params)
+      calculate_inss_discount(@proponent)
+  
+      if @proponent.save
+        redirect_to @proponent, notice: 'Proponente criado com sucesso.'
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @proponent = Proponent.find(params[:id])
+    end
+  
+    private
+  
+    def proponent_params
+      params.require(:proponent).permit(
+        :name, :document, :birthdate, :salary,
+        addresses_attributes: [:id, :street, :number, :neighborhood, :city, :state, :zip_code, :_destroy],
+        contacts_attributes: [:id, :contact_type, :value, :_destroy]
+      )
+    end
+  
+    def calculate_inss_discount(proponent)
+      proponent.inss_discount = InssCalculatorService.new(proponent.salary).calculate
+    end
+  end
+  
