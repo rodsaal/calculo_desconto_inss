f00aaff8e5c883bab0e83c796fcd55bdd9b028be
rodrigo almeida <rodrigo.rsaal@gmail.com>
2025-05-17 19:56:12 -0300

    Adding dynamic discount using javascript

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import InssController from "./inss_controller"
+application.register("inss", InssController)
